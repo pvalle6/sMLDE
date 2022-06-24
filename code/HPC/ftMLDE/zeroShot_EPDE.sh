994bad422a4f7ec0b07c96d83b49e3f1da2c07f6
pvalle6 <pvalle6@lsu.edu>
2022-06-23 21:20:25 -0500

    first part of updating glycandock scripts

--- code/HPC/ftMLDE/zeroShot_EPDE.sh
+++ code/HPC/ftMLDE/zeroShot_EPDE.sh
@@ -12,7 +12,7 @@ cd $PBS_O_WORKDIR
 #
 	#changes it to the MLDE directory 
 	# run zeroShot on workdirectory 
-python /home/pvalle6/ftMLDE/MLDE/predict_zero_shot.py --positions V39 D40 G41 V54 --models esm_msa1_t12_100M_UR50S --fasta /home/pvalle6/ftMLDE/MLDE/code/validation/basic_test_data/2GI9.fasta --alignment /home/pvalle6/ftMLDE/MLDE/code/validation/basic_test_data/GB1_Alignment.a2m  --include_conditional --batch_size 32 
+python /home/pvalle6/ftMLDE/MLDE/predict_zero_shot.py --positions V39 D40 G41 V54 --models esm_msa1_t12_100M_UR50S --fasta /home/pvalle6/ftMLDE/MLDE/code/validation/basic_test_data/2GI9.fasta --alignment /home/pvalle6/ftMLDE/MLDE/code/validation/basic_test_data/GB1_Alignment.a2m  --include_conditional --mask_col --batch_size 32 
 
 
 
