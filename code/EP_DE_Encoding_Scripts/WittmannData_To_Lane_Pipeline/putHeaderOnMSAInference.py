fb5482624e6b51e3c5a575d14ed776e78e4b919e
pvalle6 <pvalle6@lsu.edu>
2022-08-16 12:14:38 -0500

    Update putHeaderOnMSAInference.py

--- code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/putHeaderOnMSAInference.py
+++ code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/putHeaderOnMSAInference.py
@@ -2,11 +2,12 @@ import pandas as pd
 import csv 
 
 inference = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\inferenceRemoval\msa_database_without_training.csv"
-
+output_ = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\inferenceRemoval\msa_database_without_training_with_proper_headings.csv"
 columnNames= []
-df = pd.read_csv(inference,nrows = 10, header = None)
+df = pd.read_csv(inference, header = None)
 for column,k in (enumerate(df)):
 	columnNames.append(f"msa{k}")
 columnNames[0] = 'id'
 df.columns = columnNames
 print(df.head())
+df.to_csv(output_, index =False)
