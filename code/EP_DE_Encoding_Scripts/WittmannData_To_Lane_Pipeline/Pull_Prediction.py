cf4412c054b2b13903e68d196796bd235fe0faad
pvalle6 <pvalle6@lsu.edu>
2022-10-07 00:40:06 -0500

    Update Pull_Prediction.py

--- code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/Pull_Prediction.py
+++ code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/Pull_Prediction.py
@@ -2,11 +2,11 @@ import pandas as pd
 import csv 
 
 # comboFile needs to have one column with each line being the list of comboinations in the desiresd order 
-pull = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\pull_pred_test\Open_Github_Task_Files\x4Mx_req.csv"
+pull = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\pull_pred_test\Open_Github_Task_Files\x6Mx_req.csv"
 # put the msa_with combination file below 
 preds = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\pull_pred_test\Open_Github_Task_Files\fitness_scores.csv"
 
-outfile = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\pull_pred_test\Open_Github_Task_Files\x4Mx_preds.csv"
+outfile = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\pull_pred_test\Open_Github_Task_Files\x6Mx_preds.csv"
 
 def pull_preds(selection, predictions):
 	comboListDF = pd.read_csv(selection, names = ['combo'])
