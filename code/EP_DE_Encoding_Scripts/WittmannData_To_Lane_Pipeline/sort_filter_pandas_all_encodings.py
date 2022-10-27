2ec85d73b8ae44f7b7b0026c6dfd2ced1bb94580
pvalle6 <pvalle6@lsu.edu>
2022-10-26 21:39:11 -0500

    added inference HPC

--- code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/sort_filter_pandas_all_encodings.py
+++ code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/sort_filter_pandas_all_encodings.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv 
 
 # comboFile needs to have one column with each line being the list of comboinations in the desiresd order 
-comboFile = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\Sequence Encodings for GB1\x1xx\x1xx Sequences.txt"
+comboFile = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\MSA_Encodings_List\Round 3 x6Mx List Randomly Selected 1098.txt"
 # put the msa_with combination file below 
 msa_test_library = R"C:\Users\valle\OneDrive\Desktop\WitmannData\msa_transformer_with_combo.csv"
 georgiev_test_library = R"C:\Users\valle\OneDrive\Desktop\WitmannData\georgiev.csv"
@@ -12,7 +12,7 @@ one_hot_library= R"C:\Users\valle\OneDrive\Desktop\WitmannData\onehot.csv"
 # msa out put
 
 #base path that will be added onto 
-base_destination = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\Sequence Encodings for GB1\x1xx"
+base_destination = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\MSA_Encodings_List\x6Mx"
 
 
 def filter_select_encodings(selection, library, Etype):
@@ -24,6 +24,6 @@ def filter_select_encodings(selection, library, Etype):
 	df.to_csv(base_destination+Etype+R'.csv',  header=False)
 
 filter_select_encodings(comboFile,msa_test_library,R'_MSA')
-filter_select_encodings(comboFile,georgiev_test_library,R'_Georgiev')
-filter_select_encodings(comboFile,one_hot_library,R'_ONEHOT')
+#filter_select_encodings(comboFile,georgiev_test_library,R'_Georgiev')
+#filter_select_encodings(comboFile,one_hot_library,R'_ONEHOT')
 
