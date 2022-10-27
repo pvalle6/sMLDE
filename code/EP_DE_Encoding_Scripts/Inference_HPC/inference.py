6067f30a9e55eec18ea9d840d8e6181eae240996
pvalle6 <pvalle6@lsu.edu>
2022-10-26 21:49:03 -0500

    inference correct

--- code/EP_DE_Encoding_Scripts/Inference_HPC/inference.py
+++ code/EP_DE_Encoding_Scripts/Inference_HPC/inference.py
@@ -2,12 +2,14 @@ import pandas as pd
 import csv 
 
 
-msa_library = R"C:\Users\valle\OneDrive\Desktop\WitmannData\msa_transformer_with_combo.csv"
+msa_library = "/ddnA/project/jjung1/pvalle6/msa_transformer_with_combo.csv"
 #msa_test_library1 = (R"C:\Users\valle\OneDrive\Documents\EP-DE\data\inferenceRemoval\test_db.csv")
-training_combo = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\pull_pred_test\MSA_Stuff_Again\x4Mx_R2_Training_List.txt"
+training_combo_1 = "/ddnA/project/jjung1/pvalle6/inference_list/x4Mx_R2_Training_List.txt"
+training_combo_2 = "/ddnA/project/jjung1/pvalle6/inference_list/x6Mx_R2_Training_List.txt"
 #training_combo1 = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\inferenceRemoval\test_removal.txt"
 #output_ = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\inferenceRemoval\msa_database_without_training_with_proper_heading.csv"
-output_ = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\pull_pred_test\MSA_Stuff_Again\x4Mx_R3P_Sequence_Only.csv"
+output_1 = "/ddnA/project/jjung1/pvalle6/inference_list/x4Mx_R2_out.txt"
+output_2 = "/ddnA/project/jjung1/pvalle6/inference_list/x6Mx_R2_out.txt"
 
 def removeTrainAndAddHead(library, trainSeq, outputfile):
 	df = pd.read_csv(library, header = None)
@@ -25,4 +27,5 @@ def removeTrainAndAddHead(library, trainSeq, outputfile):
 	print(df.head())
 	df.to_csv(outputfile, index = False)
 
-removeTrainAndAddHead(msa_library, training_combo, output_)
+removeTrainAndAddHead(msa_library, training_combo_1, output_1)
+removeTrainAndAddHead(msa_library, training_combo_2, output_2)
