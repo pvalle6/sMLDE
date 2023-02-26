043c492f704df76f6b74fcf46569d4e76172262d
Peter <pvalle6@lsu.edu>
2023-02-25 22:33:08 -0600

    Update setup.py

--- code/sMLDE/setup.py
+++ code/sMLDE/setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 setup(
-    name='sMLDE',
+    name='smlde',
     packages=find_packages(),
     version='0.1.0',
-    description='Library for use in sMLDE',
+    description='Library for use in smlde',
     author='Peter Vallet, LSU HPC, Phillip Jung Biological Engineering',
     license='MIT',
 )
