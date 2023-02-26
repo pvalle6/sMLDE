522604747c0e40793eb8718d3d095ef2213d389a
Peter <pvalle6@lsu.edu>
2023-02-26 13:33:18 -0600

    Delete setup.py

--- code/sMLDE/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-setup(
-    name='smlde',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Library for use in smlde',
-    author='Peter Vallet, LSU HPC, Phillip Jung Biological Engineering',
-    license='MIT',
-)
