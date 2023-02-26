f243df0a2d57a00c8493c5e10d2678302d4af265
Peter <pvalle6@lsu.edu>
2023-02-25 22:33:13 -0600

    Update functions.py

--- code/sMLDE/functions.py
+++ code/sMLDE/functions.py
@@ -1,4 +1,4 @@
-"""Library for use in sMLDE"""
+"""Library for use in smlde"""
 import pandas as pd
 from pandas import DataFrame
 
@@ -6,7 +6,9 @@ from pandas import DataFrame
 def pickle_to_csv(import_file):
     """reads in the pickle file to a csv
     to be used in add combo column
-    IMPORT: pickle file to csv
+
+    Keyword arguments:
+    import_file: pickle file to csv
 
     Returns to console
     """
@@ -16,8 +18,9 @@ def pickle_to_csv(import_file):
 
 
 def filter_select_encodings(selection, library) -> DataFrame:
-    """
-    pulls encodings from Wittmann data
+    """Pulls encodings from Wittmann data
+
+    Keyword arguments:
     Selection: list of encodings
     library: selected encoding library
     Returns Pandas Dataframe
@@ -32,8 +35,9 @@ def filter_select_encodings(selection, library) -> DataFrame:
 
 
 def pull_preds(selection, predictions):
-    """
-    Outputs a csv of selected fitness prediction scores from csv file with prediction scores
+    """Outputs a csv of selected fitness prediction scores from csv file with prediction scores
+
+    Keyword arguments:
     selection: CSV of sequences
     predictions: CSV of predictions from model
     """
@@ -47,16 +51,15 @@ def pull_preds(selection, predictions):
 
 
 def add_combo_column_to_csv(inputfile, combofile):
-    """
-    Script that reads in the Encodings from
+    """Script that reads in the Encodings from
     the Wittman Dataset and adds the pickle file
     with the Combinations"""
     """ 
     takes in the msa transformer csv given in the Wittman Caltech data,
     adds in the combo in the left column
     allowing for sorting in the by sort_filter_pandas 
-    INPUTFILE: Big Wittman transformer
-    COMBOFILE: Combinations from the pickle file
+    inputfile: Big Wittman transformer
+    combofile: Combinations from the pickle file
     
     Returns Pandas DF 
     """
