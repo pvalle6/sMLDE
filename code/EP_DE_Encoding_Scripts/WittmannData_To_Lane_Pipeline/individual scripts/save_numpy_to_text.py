b8fe88a1f1b10f72e44989f850c0b65c1b0b590e
pvalle6 <pvalle6@lsu.edu>
2023-03-01 20:41:59 -0600

    Update save_numpy_to_text.py

--- code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/individual scripts/save_numpy_to_text.py
+++ code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/individual scripts/save_numpy_to_text.py
@@ -1,7 +1,7 @@
 """Script converts numpy file to txt file"""
 import numpy as np
-INPUT = R"B:\HardDrive_Files\Wittmann Data\MldeData\SimulationTrainingData\Encodings\msa_transformer.npy" #msa_transformer.npy"
-OUTPUT = R"B:\HardDrive_Files\Wittmann Data\MldeData\SimulationTrainingData\Encodings\msa_transformer.txt"#msa_transformer.csv
+INPUT = R"C:\Users\valle\OneDrive\Desktop\WitmannData\Encodings\Fitness.npy" #msa_transformer.npy"
+OUTPUT = R"C:\Users\valle\OneDrive\Desktop\WitmannData\Encodings\Fitness.csv"#msa_transformer.csv
 
 three_dim = np.load(INPUT)
 output_numpy = np.ndarray.flatten(three_dim)
