ad0e34a817c87dcd54b8ec5db40522c6fde65938
pvalle6 <pvalle6@lsu.edu>
2023-03-01 20:42:03 -0600

    Update pickle_to_csv.py

--- code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/individual scripts/pickle_to_csv.py
+++ code/EP_DE_Encoding_Scripts/WittmannData_To_Lane_Pipeline/individual scripts/pickle_to_csv.py
@@ -1,9 +1,9 @@
 """reads in the pickle file to a csv
 to be used in add combo column"""
 import pandas as pd
-IMPORT = "B:\HardDrive_Files\Wittmann Data\MldeData\SimulationTrainingData\FilteredComboToInd.pkl" #pickle file to csv
-EXPORT = "B:\HardDrive_Files\Wittmann Data\MldeData\SimulationTrainingData\FilteredComboToInd.txt" #encodings
-df = pd.read_pickle(IMPORT)
-for key, value in df.items():
-    print(key)
-pd.to_csv.
+IMPORT = R"C:\Users\valle\OneDrive\Desktop\WitmannData\Encodings\FilteredComboToInd.pkl" #pickle file to csv
+EXPORT = R"C:\Users\valle\OneDrive\Desktop\WitmannData\Encodings\FilteredComboToInd.csv" #encodings
+dic = pd.read_pickle(IMPORT)
+df = pd.DataFrame.from_dict(dic,orient='index')
+#print(df)
+df.to_csv(EXPORT)
