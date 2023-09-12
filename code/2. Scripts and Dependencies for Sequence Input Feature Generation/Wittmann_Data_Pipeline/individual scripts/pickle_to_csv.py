1d0b373f9f6b972873f84ab199df6fd74ca521b7
pvalle6 <pvalle6@lsu.edu>
2023-09-12 09:38:32 -0500

    removed names

--- code/2. Scripts and Dependencies for Sequence Input Feature Generation/Wittmann_Data_Pipeline/individual scripts/pickle_to_csv.py
+++ code/2. Scripts and Dependencies for Sequence Input Feature Generation/Wittmann_Data_Pipeline/individual scripts/pickle_to_csv.py
@@ -1,8 +1,8 @@
 """reads in the pickle file to a csv
 to be used in add combo column"""
 import pandas as pd
-IMPORT = R"C:\Users\valle\OneDrive\Desktop\WitmannData\Encodings\FilteredComboToInd.pkl" #pickle file to csv
-EXPORT = R"C:\Users\valle\OneDrive\Desktop\WitmannData\Encodings\FilteredComboToInd.csv" #encodings
+IMPORT = None #pickle file to csv
+EXPORT = None #encodings
 dic = pd.read_pickle(IMPORT)
 df = pd.DataFrame.from_dict(dic,orient='index')
 #print(df)
--- code/2. Scripts and Dependencies for Sequence Input Feature Generation/Wittmann_Data_Pipeline/individual scripts/save_numpy_to_text.py
+++ code/2. Scripts and Dependencies for Sequence Input Feature Generation/Wittmann_Data_Pipeline/individual scripts/save_numpy_to_text.py
@@ -1,7 +1,7 @@
 """Script converts numpy file to txt file"""
 import numpy as np
-INPUT = R"C:\Users\valle\OneDrive\Desktop\WitmannData\Encodings\Fitness.npy" #msa_transformer.npy"
-OUTPUT = R"C:\Users\valle\OneDrive\Desktop\WitmannData\Encodings\Fitness.csv"#msa_transformer.csv
+INPUT = None #msa_transformer.npy"
+OUTPUT = None #msa_transformer.csv
 
 three_dim = np.load(INPUT)
 output_numpy = np.ndarray.flatten(three_dim)
